1003e60e3a8e90c40616bccf793aa66bc0a6ee76
Sirinat Paphatsirinatthi <cpfapp@gmail.com>
2023-12-13 15:14:03 +0700

    Update APIService.cs

--- Services/APIService.cs
+++ Services/APIService.cs
@@ -12,7 +12,7 @@ namespace DotNetCoreWebAPI.Services {
         }
 
         public string ConnectionToDatabase(){
-            return "Hello World with Hello World1: "+_dbOptions.DBURL+", DB_USERNAME: "+_dbOptions.DBUserName+", DB_PASSWORD: "+_dbOptions.DBPassword;
+            return "Hello World: "+_dbOptions.DBURL+", DB_USERNAME: "+_dbOptions.DBUserName+", DB_PASSWORD: "+_dbOptions.DBPassword;
         }
     }
 }
